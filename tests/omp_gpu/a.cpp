69b4ec0e52d1e64a9342c7d6b69f83c9982b8999
Lucas Morais Freire <lucasmoraisf@gmail.com>
2024-11-03 06:02:32 +0000

    maybe I have found the solution

---
+++ tests/omp_gpu/a.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << sizeof(size_t) << '\n';
+
+    return 0;
+}
--- tests/omp_gpu/main.cpp
+++ tests/omp_gpu/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <omp.h>
-#define SZ 10
+#define SZ 100000000
 
 template <typename T>
 void print_array(T* arr, int sz) {
@@ -16,77 +16,58 @@ void print_array(T* arr, int sz) {
     std::cout << "]\n";
 }
 
-void target_enter(int* arr, int sz) {
-    #pragma omp target enter data map(to:arr[0:sz])
-}
-
-void target_get_data(int* arr, int sz) {
-    #pragma omp target update from(arr[0:sz])
-}
-
-void target_set_data(int* arr, int sz) {
-    #pragma omp target update to(arr[0:sz])
-}
-
-void target_release_data(int* arr, int sz) {
-    #pragma omp target exit data map(release:arr[0:sz])
-}
-
 struct obj {
 public:
-    int* x;
-    int* y;
-    int* ax;
-    int* ay;
-    size_t sz;
+    int* _x;
+    int* _y;
+    size_t _sz;
+
     obj(size_t sz) {
-        this->sz = sz;
-        x = new int[sz];
-        y = new int[sz];
+        _sz = sz;
+        _x = new int[sz];
+        _y = new int[sz];
 
-        #pragma omp target enter data map(alloc:x[0:sz], y[0:sz], ax, ay, sz)
-        #pragma omp target update to(sz)
+        #pragma omp target enter data map(alloc: _x[0:_sz], _y[0:_sz], _sz)
     }
+
     ~obj() {
-        #pragma omp target exit data map(release:x[0:sz], y[0:sz], ax, ay, sz)
+        #pragma omp target exit data map(release: _x[0:_sz], _y[0:_sz], _sz)
 
-        delete[] x;
-        delete[] y;
+        delete[] _x;
+        delete[] _y;
     }
 
-    void computation() {
-        #pragma omp target teams distribute parallel for simd default(none) shared(x, y, sz)
-        for (size_t i = 0; i < sz; i++) {
-            y[i] = x[i] + 2;
-        }
-
-        #pragma omp target
-        {
-            int* aux = x;
-            x = y;
-            y = aux;
-        }
-    }
 };
+
+void computation(int *&x, int *&y, size_t &sz) {
+    #pragma omp target teams distribute parallel for simd
+    for (size_t i = 0; i < sz; i++) {
+        y[i] = x[i] + 10;
+    }
+
+    std::swap<int*>(x, y);
+}
 
 
 int main() {
     obj test(SZ);
     for (size_t i = 0; i < SZ; i++) {
-        test.x[i] = i;
+        test._x[i] = i;
     }
-    #pragma omp target enter data map(to:test)
     
-    #pragma omp target update to(test.x[0:SZ])
+    #pragma omp target update to(test._x[0:SZ], test._sz)
+    
+    computation(test._x, test._y, test._sz);
+    computation(test._x, test._y, test._sz);
+    computation(test._x, test._y, test._sz);
+    computation(test._x, test._y, test._sz);
 
-    test.computation();
+    #pragma omp target update from(test._x[0:SZ], test._y[0:SZ])
 
-    #pragma omp target update from(test.x[0:SZ], test.y[0:SZ])
-
-    std::cout << "x: ";
-    print_array(test.x, SZ);
-    std::cout << "y: ";
-    print_array(test.y, SZ);
+    // std::cout << "x: ";
+    // print_array(test._x, SZ);
+    // std::cout << "y: ";
+    // print_array(test._y, SZ);
 
     #ifdef _OPENMP
     std::cout << "num devices: " << omp_get_num_devices() << ", On device: " << omp_get_default_device() << "\n";
